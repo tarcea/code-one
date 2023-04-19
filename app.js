e87530fb98fc80143354f2c8ff9ce9e1de61334b
tarcea <tarcea@gmail.com>
2023-04-19 09:29:28 +0200

    expose get route with hi message

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hi' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
