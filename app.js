ee156f7e088c5e5fed3e4613d680ff1174e3c761
tarcea <tarcea@gmail.com>
2023-04-19 09:37:56 +0200

    post route added

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Hi' });
+});
+
+app.post('/', (req, res) => {
+  const { name } = req.body;
+  console.log({ name });
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
